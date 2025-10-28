879c94c3e3040756b1fff8401a1854e1545611b4
David Tweedle <david.aig.tweedle@gmail.com>
2025-10-28 13:29:45 -0400

    second try fixing indents

--- jax/submission.py
+++ jax/submission.py
@@ -271,12 +271,12 @@ def create_svd_args(
 
 class ScaleByLowRankOrthogonalUpdateState(NamedTuple):
     """State for the Low Rank Orthogonal Update algorithm.
-  """
-  step: chex.Array          # number of steps
-  momentum: Any             # Pytree storing momentum of parameter
-  krylov_iter: chex.Array   # number of krylov iterations
-  key: Any                  # random key Pytree
-  rank: Any                 # Pytree containing rank parameter for low rank svd
+    """
+    step: chex.Array          # number of steps
+    momentum: Any             # Pytree storing momentum of parameter
+    krylov_iter: chex.Array   # number of krylov iterations
+    key: Any                  # random key Pytree
+    rank: Any                 # Pytree containing rank parameter for low rank svd
 
 
 def low_rank_orthogonal_update(
@@ -296,162 +296,161 @@ def low_rank_orthogonal_update(
 
     Returns:
         The corresponding `GradientTransformation`.
-  """
-  param_labels = create_param_labels()
-  transform_shapes = create_transform_shapes()
-  inv_transform_shapes = create_inv_transform_shapes()
-  return optax.partition(
-          transforms={
-              'low_rank_orthogonal_update': optax.chain(
-                  transform_shapes,
-                  scale_by_low_rank_orthogonal_update(
-                      beta1=beta1,
-                      krylov_iter=krylov_iter,
-                      rank_type=rank_type,
-                      rank_val=rank_val,
-                      eps=eps
-                      ),
-                  inv_transform_shapes,
-                  optax.add_decayed_weights(weight_decay, mask),
-                  optax.scale_by_learning_rate(lr)
-                  ),
-              'adam': optax.nadamw(
-                  learning_rate=lr,
-                  beta1=beta1,
-                  beta2=beta2,
-                  eps=eps,
-                  eps_root=eps_root,
-                  weight_decay=weight_decay,
-                  mask=mask
-                  )
-              },
-          param_labels=param_labels
-          )
+    """
+    param_labels = create_param_labels()
+    transform_shapes = create_transform_shapes()
+    inv_transform_shapes = create_inv_transform_shapes()
+    return optax.partition(
+            transforms={
+                'low_rank_orthogonal_update': optax.chain(
+                    transform_shapes,
+                    scale_by_low_rank_orthogonal_update(
+                        beta1=beta1,
+                        krylov_iter=krylov_iter,
+                        rank_type=rank_type,
+                        rank_val=rank_val,
+                        eps=eps
+                        ),
+                    inv_transform_shapes,
+                    optax.add_decayed_weights(weight_decay, mask),
+                    optax.scale_by_learning_rate(lr)
+                    ),
+                'adam': optax.nadamw(
+                    learning_rate=lr,
+                    beta1=beta1,
+                    beta2=beta2,
+                    eps=eps,
+                    eps_root=eps_root,
+                    weight_decay=weight_decay,
+                    mask=mask
+                    )
+                },
+            param_labels=param_labels
+            )
 
 
 
 
-  def scale_by_low_rank_orthogonal_update(
-          key: int,
-          beta1: float = 0.9, 
-          krylov_iter: int = 2, 
-          rank_type: str = 'sqrt', 
-          rank_val: Optional[int] = None,
-          eps=1e-8,
-          ):
-      """
+def scale_by_low_rank_orthogonal_update(
+      key: int,
+      beta1: float = 0.9, 
+      krylov_iter: int = 2, 
+      rank_type: str = 'sqrt', 
+      rank_val: Optional[int] = None,
+      eps=1e-8,
+      ):
+    """
     Scale gradients using low-rank SVD-based orthogonal updates
 
     This optimizer maintains momentum and applies preconditioned updates
     via randomized SVD.
 
     Args:
-        key: Random key for sketching method
-        beta1: Momentum parameter (default: 0.9)
-        krylov_iter: Number of Krylov iterations for svd range finding (default: 2)
-        rank_type: How to determine SVD rank - 'sqrt' or 'constant' (TODO: add 'log')
-        rank: Fixed rank if rank_type='constant'
-        eps: Small constant for numerical stability (default: 1e-8)
+    key: Random key for sketching method
+    beta1: Momentum parameter (default: 0.9)
+    krylov_iter: Number of Krylov iterations for svd range finding (default: 2)
+    rank_type: How to determine SVD rank - 'sqrt' or 'constant' (TODO: add 'log')
+    rank: Fixed rank if rank_type='constant'
+    eps: Small constant for numerical stability (default: 1e-8)
 
     Returns:
-        GradientTransformation: Optax gradient transformation
+    GradientTransformation: Optax gradient transformation
 
     Note:
-        Assumes params are matrices
+    Assumes params are matrices
     """
-  def init_fn(params):
-      momentum = jax.tree.map(
-              jnp.zeros_like, params
-              )
-      key, rank = create_svd_args(params, key, rank_type, rank)
-      return ScaleByLowRankOrthogonalUpdateState(
-              step=jnp.zeros([], jnp.int32),
-              momentum=momentum,
-              krylov_iter=krylov_iter,
-              rank=rank,
-              key=key
-              )
+    def init_fn(params):
+        momentum = jax.tree.map(
+                jnp.zeros_like, params
+                )
+        key, rank = create_svd_args(params, key, rank_type, rank)
+        return ScaleByLowRankOrthogonalUpdateState(
+                step=jnp.zeros([], jnp.int32),
+                momentum=momentum,
+                krylov_iter=krylov_iter,
+                rank=rank,
+                key=key
+                )
 
-  def update_fn(updates, state, params=None):
-      del params
-      step_inc = state.step + 1
-      momentum = _update_moment(updates, state.momentum, beta1, 1)
-      key, consumable_key = jax.tree.map(jax.random.split, state.key)
-      low_rank_momentum = jax.tree.map(
-              linalg.svd_lowrank,
-              momentum,
-              consumable_key,
-              state.rank,
-              state.krylov_iter
-              )
-      return low_rank_momentum, ScaleByLowRankOrthogonalUpdateState(
-              step=step_inc,
-              momentum=momentum,
-              key=key,
-              krylov_iter=krylov_iter,
-              rank=state.rank
-              )
-  return optax.GradientTransformation(init_fn, update_fn)
+    def update_fn(updates, state, params=None):
+        del params
+        step_inc = state.step + 1
+        momentum = _update_moment(updates, state.momentum, beta1, 1)
+        key, consumable_key = jax.tree.map(jax.random.split, state.key)
+        low_rank_momentum = jax.tree.map(
+                linalg.svd_lowrank,
+                momentum,
+                consumable_key,
+                state.rank,
+                state.krylov_iter
+                )
+        return low_rank_momentum, ScaleByLowRankOrthogonalUpdateState(
+                step=step_inc,
+                momentum=momentum,
+                key=key,
+                krylov_iter=krylov_iter,
+                rank=state.rank
+                )
+    return optax.GradientTransformation(init_fn, update_fn)
 
 
 
 def _update_moment(updates, moments, decay, order):
     """Compute the exponential moving average of the `order`-th moment."""
-  return jax.tree.map(
+    return jax.tree.map(
           lambda g, t: (1 - decay) * (g ** order) + decay * t, updates, moments)
 
 
-  def train_step(workload,
-                 opt_update_fn,
-                 model_state,
-                 optimizer_state,
-                 current_param_container,
-                 batch,
-                 rng,
-                 grad_clip,
-                 label_smoothing,
-                 dropout_rate,
-                 ):
+def train_step(workload,
+             opt_update_fn,
+             model_state,
+             optimizer_state,
+             current_param_container,
+             batch,
+             rng,
+             grad_clip,
+             label_smoothing,
+             dropout_rate,
+             ):
 
-      def _loss_fn(params):
-          """Loss function used for training."""
-    logits, new_model_state = workload.model_fn(
-            params,
-            batch,
-            model_state,
-            spec.ForwardPassMode.TRAIN,
-            rng,
-            update_batch_norm=True,
-            dropout_rate=dropout_rate,
-            )
-    loss_dict = workload.loss_fn(
-            label_batch=batch['targets'],
-            logits_batch=logits,
-            mask_batch=batch.get('weights'),
-            label_smoothing=label_smoothing)
-    summed_loss = loss_dict['summed']
-    n_valid_examples = loss_dict['n_valid_examples']
-    return summed_loss, (n_valid_examples, new_model_state)
+    def _loss_fn(params):
+        logits, new_model_state = workload.model_fn(
+                params,
+                batch,
+                model_state,
+                spec.ForwardPassMode.TRAIN,
+                rng,
+                update_batch_norm=True,
+                dropout_rate=dropout_rate,
+                )
+        loss_dict = workload.loss_fn(
+                label_batch=batch['targets'],
+                logits_batch=logits,
+                mask_batch=batch.get('weights'),
+                label_smoothing=label_smoothing)
+        summed_loss = loss_dict['summed']
+        n_valid_examples = loss_dict['n_valid_examples']
+        return summed_loss, (n_valid_examples, new_model_state)
 
-grad_fn = jax.value_and_grad(_loss_fn, has_aux=True)
-  (summed_loss, (n_valid_examples, new_model_state)), grad = grad_fn(
+    grad_fn = jax.value_and_grad(_loss_fn, has_aux=True)
+    (summed_loss, (n_valid_examples, new_model_state)), grad = grad_fn(
           current_param_container)
-  # Get correct global mean loss and grad.
-  loss = summed_loss / n_valid_examples
-  grad = jax.tree.map(lambda x: x / n_valid_examples, grad)
+    # Get correct global mean loss and grad.
+    loss = summed_loss / n_valid_examples
+    grad = jax.tree.map(lambda x: x / n_valid_examples, grad)
 
-  grad_norm = jnp.sqrt(
+    grad_norm = jnp.sqrt(
           sum(jnp.sum(g**2) for g in jax.tree_util.tree_leaves(grad)))
 
-  if grad_clip is not None:
-      grad_scaling_factor = grad_clip / (grad_norm + _GRAD_CLIP_EPS)
+    if grad_clip is not None:
+        grad_scaling_factor = grad_clip / (grad_norm + _GRAD_CLIP_EPS)
     grad_scaling_factor = jax.lax.clamp(min=0.0, x=grad_scaling_factor, max=1.0)
     grad = jax.tree.map(lambda x: x * grad_scaling_factor, grad)
 
-  updates, new_optimizer_state = opt_update_fn(grad, optimizer_state,
+    updates, new_optimizer_state = opt_update_fn(grad, optimizer_state,
                                                current_param_container)
-  updated_params = optax.apply_updates(current_param_container, updates)
-  return new_optimizer_state, updated_params, new_model_state, loss, grad_norm
+    updated_params = optax.apply_updates(current_param_container, updates)
+    return new_optimizer_state, updated_params, new_model_state, loss, grad_norm
 
 
 def update_params(
@@ -468,75 +467,75 @@ def update_params(
         rng: spec.RandomState,
         train_state: Optional[Dict[str, Any]] = None) -> spec.UpdateReturn:
     """Return (updated_optimizer_state, updated_params, updated_model_state)."""
-  del current_params_types
-  del loss_type
-  del train_state
-  del eval_results
-  del hyperparameters
+    del current_params_types
+    del loss_type
+    del train_state
+    del eval_results
+    del hyperparameters
 
-  hyperparameters = HPARAMS
+    hyperparameters = HPARAMS
 
-  optimizer_state, opt_update_fn = optimizer_state
-  if 'label_smoothing' in hyperparameters:
-      label_smoothing = hyperparameters['label_smoothing']
-  else:
-      label_smoothing = 0.0
-  if 'grad_clip' in hyperparameters:
-      grad_clip = hyperparameters['grad_clip']
-  else:
-      grad_clip = None
-  dropout_rate = hyperparameters['dropout_rate']
+    optimizer_state, opt_update_fn = optimizer_state
+    if 'label_smoothing' in hyperparameters:
+        label_smoothing = hyperparameters['label_smoothing']
+    else:
+        label_smoothing = 0.0
+    if 'grad_clip' in hyperparameters:
+        grad_clip = hyperparameters['grad_clip']
+    else:
+        grad_clip = None
+    dropout_rate = hyperparameters['dropout_rate']
 
-  # mesh = jax.sharding.Mesh(jax.devices(), ('batch'))
-  replicated = jax_sharding_utils.get_replicate_sharding()
-  sharded = (
-          jax_sharding_utils.get_batch_dim_sharding()
-          )
-  arg_shardings = (
-          replicated, #model_state
-          replicated, #optimizer_state # change to optimizer sharding eventually
-          replicated, # current_param_container
-          sharded, # batch
-          replicated, # per_device_rngs
-          replicated, # grad_clip
-          replicated, #label_smoothing
-          replicated, #dropout_rate
-          )
-  out_shardings = (
-          replicated, # new_optimizer_state # maybe sharded eventually
-          replicated, # updated_params
-          replicated, # new_model_state
-          replicated, # loss
-          replicated, # grad_norm
-          )
-  jitted_train_step = jax.jit(
-          train_step,
-          static_argnums=(0, 1),
-          donate_argnums=(2, 3, 4),
-          in_shardings=arg_shardings,
-          out_shardings=out_shardings,
-          )
-  outputs = jitted_train_step(workload,
-                              opt_update_fn,
-                              model_state,
-                              optimizer_state,
-                              current_param_container,
-                              batch,
-                              rng,
-                              grad_clip,
-                              label_smoothing,
-                              dropout_rate,
-                              )
-  new_optimizer_state, new_params, new_model_state, loss, grad_norm = outputs
+    # mesh = jax.sharding.Mesh(jax.devices(), ('batch'))
+    replicated = jax_sharding_utils.get_replicate_sharding()
+    sharded = (
+            jax_sharding_utils.get_batch_dim_sharding()
+            )
+    arg_shardings = (
+            replicated, #model_state
+            replicated, #optimizer_state # change to optimizer sharding eventually
+            replicated, # current_param_container
+            sharded, # batch
+            replicated, # per_device_rngs
+            replicated, # grad_clip
+            replicated, #label_smoothing
+            replicated, #dropout_rate
+            )
+    out_shardings = (
+            replicated, # new_optimizer_state # maybe sharded eventually
+            replicated, # updated_params
+            replicated, # new_model_state
+            replicated, # loss
+            replicated, # grad_norm
+            )
+    jitted_train_step = jax.jit(
+            train_step,
+            static_argnums=(0, 1),
+            donate_argnums=(2, 3, 4),
+            in_shardings=arg_shardings,
+            out_shardings=out_shardings,
+            )
+    outputs = jitted_train_step(workload,
+                                opt_update_fn,
+                                model_state,
+                                optimizer_state,
+                                current_param_container,
+                                batch,
+                                rng,
+                                grad_clip,
+                                label_smoothing,
+                                dropout_rate,
+                                )
+    new_optimizer_state, new_params, new_model_state, loss, grad_norm = outputs
 
-  # Log loss, grad_norm.
-  if global_step % 100 == 0 and workload.metrics_logger is not None:
-      workload.metrics_logger.append_scalar_metrics(
-              {
-                  'loss': loss,
-                  'grad_norm': grad_norm,
-                  }, global_step)
-  return (new_optimizer_state, opt_update_fn), new_params, new_model_state
+    # Log loss, grad_norm.
+    if global_step % 100 == 0 and workload.metrics_logger is not None:
+        workload.metrics_logger.append_scalar_metrics(
+                {
+                    'loss': loss,
+                    'grad_norm': grad_norm,
+                    }, global_step)
+    return (new_optimizer_state, opt_update_fn), new_params, new_model_state
 
 
 

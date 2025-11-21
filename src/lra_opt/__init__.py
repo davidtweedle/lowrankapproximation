a9327b4afed41ccb6cfe5ebd0e2144d3ba2efacb
David Tweedle <david.aig.tweedle@gmail.com>
2025-11-21 10:56:58 -0500

    fixed init

--- src/lra_opt/__init__.py
+++ src/lra_opt/__init__.py
@@ -2,12 +2,10 @@ from .optimizer import (
         low_rank_orthogonal_update,
         create_param_labels,
         scale_by_low_rank_orthogonal_update,
-        AugmentedShapeInfo,
         )
 
 __all__ = [
         "low_rank_orthogonal_update",
         "create_param_labels",
         "scale_by_low_rank_orthogonal_update",
-        "AugmentedShapeInfo",
         ]
